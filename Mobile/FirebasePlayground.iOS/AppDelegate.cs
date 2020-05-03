7774bbd8b4d684a3c42e2a8ed3ee3bc8fb14097d
Gagik Kyurkchyan <gagik.kyurkchyan@gmail.com>
2020-05-03 12:47:20 +0400

    Integrated Firebase iOS push notifications

--- Mobile/FirebasePlayground.iOS/AppDelegate.cs
+++ Mobile/FirebasePlayground.iOS/AppDelegate.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using Firebase.CloudMessaging;
+using Firebase.Crashlytics;
 using Foundation;
 using UIKit;
+using UserNotifications;
 
 namespace FirebasePlayground.iOS
 {
@@ -11,7 +13,7 @@ namespace FirebasePlayground.iOS
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IUNUserNotificationCenterDelegate, IMessagingDelegate
     {
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
@@ -24,8 +26,74 @@ namespace FirebasePlayground.iOS
         {
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
-
+            Firebase.Core.App.Configure();
+            Crashlytics.Configure();
+            //Linker please include
+            var unusedLinkerPleaseInclude = Firebase.Core.Configuration.SharedInstance;
+            RegisterRemoteNotifications();
             return base.FinishedLaunching(app, options);
         }
+        
+        private void RegisterRemoteNotifications()
+        {
+            // Register your app for remote notifications.
+            if(UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                // For iOS 10 display notification (sent via APNS)
+                UNUserNotificationCenter.Current.Delegate = this;
+
+                var authOptions = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
+                UNUserNotificationCenter.Current.RequestAuthorization(authOptions, (granted, error) => { Console.WriteLine(granted); });
+            }
+            else
+            {
+                // iOS 9 or before
+                var allNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound;
+                var settings = UIUserNotificationSettings.GetSettingsForTypes(allNotificationTypes, null);
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
+
+            UIApplication.SharedApplication.RegisterForRemoteNotifications();
+
+            Messaging.SharedInstance.Delegate = this;
+        }
+        
+        public override void ReceivedRemoteNotification (UIApplication application, NSDictionary userInfo)
+        {
+            // If you are receiving a notification message while your app is in the background,
+            // this callback will not be fired till the user taps on the notification launching the application.
+            // TODO: Handle data of notification
+
+            // With swizzling disabled you must let Messaging know about the message, for Analytics
+            //Messaging.SharedInstance.AppDidReceiveMessage (userInfo);
+
+            // Print full message.
+            Console.WriteLine (userInfo);
+        }
+
+        public override void DidReceiveRemoteNotification (UIApplication application, NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
+        {
+            // If you are receiving a notification message while your app is in the background,
+            // this callback will not be fired till the user taps on the notification launching the application.
+            // TODO: Handle data of notification
+
+            // With swizzling disabled you must let Messaging know about the message, for Analytics
+            // Messaging.SharedInstance.AppDidReceiveMessage (userInfo);
+
+            // Print full message.
+            Console.WriteLine (userInfo);
+
+            completionHandler (UIBackgroundFetchResult.NewData);
+        }
+        
+        [Export("messaging:didReceiveRegistrationToken:")]
+        public void DidReceiveRegistrationToken(Messaging messaging, string fcmToken)
+        {
+            Console.WriteLine($"Firebase registration token: {fcmToken}");
+
+            // TODO: If necessary send token to application server.
+            // Note: This callback is fired at each app startup and whenever a new token is generated.
+        }
+
     }
 }
